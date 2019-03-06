6baec06156f839dc665e39c2b6a3e93dd1b67d50
Shubhashis Roy <iamdipta@gmail.com>
2019-03-07 00:06:34 +0600

    updated port

--- client/src/Container/Feed/Feed.jsx
+++ client/src/Container/Feed/Feed.jsx
@@ -9,7 +9,7 @@ class Feed extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: dummy,
+      posts: [],
       sortOrder: 'Most Voted',
       loading: true
     };
