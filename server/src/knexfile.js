492ab48820fbeb604edf1c791f74f6b5428ca39f
Shubhashis Roy <iamdipta@gmail.com>
2019-03-11 00:33:35 +0600

    added production config to knex

--- server/src/knexfile.js
+++ server/src/knexfile.js
@@ -16,5 +16,20 @@ module.exports = {
       max: 1
     },
     debug
+  },
+  production: {
+    client: 'pg',
+    version: '7.2',
+    connection: {
+      host: 'sapien.crffscfa8eeu.us-east-2.rds.amazonaws.com',
+      user: 'dipta007',
+      password: 'dipta007',
+      database: 'sapien_test',
+      timezone: 'UTC'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
   }
 };
