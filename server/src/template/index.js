4b498255beb85f5493d053c603a2bcd69b9576c8
Shubhashis Roy <iamdipta@gmail.com>
2019-03-10 14:03:40 +0600

    removed template from backend

--- server/src/template/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const temp = require('./temp');
-
-const app = express();
-app.get('/temp', temp.logIn);
-
-module.exports = app;
--- server/src/template/temp.js
+++
@@ -1,7 +0,0 @@
-const tmp = (req, res) => {
-  res.status(200).send('logged in');
-};
-
-module.exports = {
-  tmp
-};
